3003e714491477d217f4a2773694b913175e1388
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-06-15 22:21:26 +0200

    Added admin.py file in glossaries app to register the models in the admin site

---
+++ terminator/glossaries/admin.py
@@ -0,0 +1,10 @@
+from glossaries.models import *
+from django.contrib import admin
+
+admin.site.register(Language)
+admin.site.register(Glossary)
+admin.site.register(Concept)
+admin.site.register(Translation)
+admin.site.register(Definition)
+
+
