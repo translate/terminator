0c624aebfcba12f4611f80ce8e320d86118bf7c2
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-06-16 00:15:28 +0200

    Added Translation admin to tweak the admin page for translations

--- terminator/glossaries/admin.py
+++ terminator/glossaries/admin.py
@@ -4,7 +4,15 @@ from django.contrib import admin
 admin.site.register(Language)
 admin.site.register(Glossary)
 admin.site.register(Concept)
-admin.site.register(Translation)
+
+class TranslationAdmin(admin.ModelAdmin):
+    fields = ['concept', 'language', 'translation_text', 'process_status']
+    list_display = ('translation_text', 'language', 'concept')
+    ordering = ('concept',)
+    list_filter = ['language', 'concept']
+    search_fields = ['translation_text']
+
+admin.site.register(Translation, TranslationAdmin)
 admin.site.register(Definition)
 
 
