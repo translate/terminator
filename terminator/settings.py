8bdeb1616ba9beb39d889f9c605d89e4ab5ef1fd
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-06-18 21:11:23 +0200

    Setting global and app template dirs in settings in a dynamic way

--- terminator/settings.py
+++ terminator/settings.py
@@ -1,4 +1,5 @@
 # Django settings for terminator project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +107,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\', '/'),
+    os.path.join(os.path.join(os.path.dirname(__file__), 'glossaries'), 'templates').replace('\\', '/'),
 )
 
 INSTALLED_APPS = (
