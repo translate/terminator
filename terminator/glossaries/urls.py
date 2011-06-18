ab41e2eff1b257e1bd1e66a43b4ebac14fa69696
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-06-18 22:12:45 +0200

    Added urlconf entry using generic view, and template to show the glossary list

--- terminator/glossaries/urls.py
+++ terminator/glossaries/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls.defaults import patterns
+from django.views.generic import ListView
+from glossaries.models import Glossary
 
 urlpatterns = patterns('glossaries.views',
     (r'^$', 'terminator_index'),
+    (r'^glossaries/$', ListView.as_view(
+        model=Glossary,
+        context_object_name="glossary_list",
+    )),
 )
 
