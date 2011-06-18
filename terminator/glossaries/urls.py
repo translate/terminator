56c220ecfa7edbfc1b14986bea843ba194e23814
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-06-18 22:15:55 +0200

    Added urlconf entry using generic view, and template to show the concept list

--- terminator/glossaries/urls.py
+++ terminator/glossaries/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns
 from django.views.generic import ListView
-from glossaries.models import Glossary
+from glossaries.models import Glossary, Concept
 
 urlpatterns = patterns('glossaries.views',
     (r'^$', 'terminator_index'),
@@ -9,5 +9,9 @@ urlpatterns = patterns('glossaries.views',
         context_object_name="glossary_list",
     )),
     (r'^glossaries/(?P<glossary_id>\d+)/$', 'glossary_detail'),
+    (r'^concepts/$', ListView.as_view(
+        model=Concept,
+        context_object_name="concept_list",
+    )),
 )
 
