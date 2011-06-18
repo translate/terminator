4a529d1a125252fea74ae2442c7089089009569f
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-06-18 21:13:53 +0200

    Enabling the staticfiles app

--- terminator/settings.py
+++ terminator/settings.py
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     #'django.contrib.sites',
     #'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
