421629753d9bf20b0d3df66d582a44c39a18b1fa
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-07-06 21:39:11 +0200

    Adding extra validation in Translation admin page for checking several grammatical gender issues

--- project/terminator/forms.py
+++ project/terminator/forms.py
@@ -27,6 +27,28 @@ class TerminatorTranslationAdminForm(forms.ModelForm):
                 # This field is no longer valid. Remove it from the cleaned data.
                 del cleaned_data["part_of_speech"]
         
+        grammatical_gender = cleaned_data.get("grammatical_gender")
+        # Check that Grammatical gender is only specified when is given a Part of speech
+        if not part_of_speech and grammatical_gender:
+            msg = u"Don't specify a Grammatical gender without specifying a Part of speech."
+            self._errors["grammatical_gender"] = self.error_class([msg])
+            # This field is no longer valid. Remove it from the cleaned data.
+            del cleaned_data["grammatical_gender"]
+        
+        # Check that the Part of speech allows specifying a Grammatical 
+        # gender for the chosen language, and check that the chosen language
+        # allows using the specified Grammatical gender
+        if language and part_of_speech and grammatical_gender:
+            msg = u""
+            if not part_of_speech.allows_grammatical_gender_for_language(language):
+                msg = u"The specified Part of speech doesn't allow specifying a Grammatical gender for the chosen language."
+            if not language.allows_grammatical_gender(grammatical_gender):
+                msg += u"The chosen language doesn't allow specifying this Grammatical gender."
+            if msg:
+                self._errors["grammatical_gender"] = self.error_class([msg])
+                # This field is no longer valid. Remove it from the cleaned data.
+                del cleaned_data["grammatical_gender"]
+        
         # Always return the full collection of cleaned data.
         return cleaned_data
 
