ebe74a3ed92678a875a470e77a16b8ddf5d7e7d8
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-07-06 17:18:55 +0200

    Changes in SearchForm form

--- project/terminator/forms.py
+++ project/terminator/forms.py
@@ -1,7 +1,8 @@
+# -*- coding: UTF-8 -*-
 from django import forms
 
 
 class SearchForm(forms.Form):
-    search_string = forms.CharField(max_length=100)
+    search_string = forms.CharField(max_length=100, min_length=2, required=False)
 
 
