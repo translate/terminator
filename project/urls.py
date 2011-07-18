00d093d596f6dd2faf4e734dda71fb6ed46283d8
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-07-18 22:43:14 +0200

    Minor cosmetic changes in project urls.py

--- project/urls.py
+++ project/urls.py
@@ -6,14 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'terminator.views.home', name='home'),
-    # url(r'^terminator/', include('terminator.foo.urls')),
-
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^project/', include('project.foo.urls')),
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^', include('terminator.urls')),
 )
-
