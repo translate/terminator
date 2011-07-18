2c491e68453d19fc40b671589733d14187de1b06
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-07-18 21:54:21 +0200

    Adding url entry and template to manage the login

--- project/terminator/urls.py
+++ project/terminator/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls.defaults import patterns
+from django.contrib.auth.views import login
 from terminator.views import TerminatorListView, TerminatorDetailView
 from terminator.models import Glossary, Concept, Proposal
 
 urlpatterns = patterns('terminator.views',
     (r'^$', 'terminator_index'),
+    (r'^accounts/login/$', login),
     (r'^glossaries/$', TerminatorListView.as_view(
         model=Glossary,
         context_object_name="glossary_list",
