a8b425a4d4787b8dbf8ee33754a08b2117f99b4c
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-07-26 23:03:39 +0200

    Adding restrictions in TranslationAdmin in order to show only concepts belonging to glossaries for which the user has terminologist permission in the concept field

--- project/terminator/admin.py
+++ project/terminator/admin.py
@@ -135,6 +135,12 @@ class TranslationAdmin(admin.ModelAdmin):
             return qs
         inner_qs = get_objects_for_user(request.user, ['is_terminologist_in_this_glossary'], Glossary, False)
         return qs.filter(concept__glossary__in=inner_qs)
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "concept":
+            inner_qs = get_objects_for_user(request.user, ['is_terminologist_in_this_glossary'], Glossary, False)
+            kwargs["queryset"] = Concept.objects.filter(glossary__in=inner_qs)
+        return super(TranslationAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Translation, TranslationAdmin)
 
