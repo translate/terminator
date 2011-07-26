179d0ac9c70245c81cf3a4b09ad5b4cd6ca9bc5d
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-07-26 23:10:38 +0200

    Overriding the ContextSentenceAdmin queryset method to show only context sentences belonging to translations that belong to concepts that belong to glossaries for which the user has terminologist permission

--- project/terminator/admin.py
+++ project/terminator/admin.py
@@ -270,6 +270,13 @@ class ContextSentenceAdmin(admin.ModelAdmin):
     list_display = ('text', 'translation')
     ordering = ('translation',)
     list_filter = ['translation']
+    
+    def queryset(self, request):
+        qs = super(ContextSentenceAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        inner_qs = get_objects_for_user(request.user, ['is_terminologist_in_this_glossary'], Glossary, False)
+        return qs.filter(translation__concept__glossary__in=inner_qs)
 
 admin.site.register(ContextSentence, ContextSentenceAdmin)
 
