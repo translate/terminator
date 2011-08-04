7b8bf16b59395d7e16d0d8cf9c9284b5b8905d83
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-08-04 17:08:43 +0200

    Comments: uncommenting sites app line in INSTALLED_APPS and adding comments app

--- project/settings.py
+++ project/settings.py
@@ -121,13 +121,14 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
+    'django.contrib.sites',
     #'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django.contrib.comments',
     'guardian',
     'project.terminator',
 )
