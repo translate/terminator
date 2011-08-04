4f211d8772a981b7a9ec15e81adefd5ec114eb64
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-08-04 17:09:33 +0200

    Comments: registering comments app urls

--- project/urls.py
+++ project/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
     
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^', include('terminator.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^', include('terminator.urls')),
 )
