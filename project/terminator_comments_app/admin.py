733cbe34334ed82b0d3219ff27f62604c1918104
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-08-09 16:54:44 +0200

    Comments: registering custom comments app in admin site

---
+++ project/terminator_comments_app/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.comments.admin import CommentsAdmin
+from terminator_comments_app.models import TerminatorComment
+
+class TerminatorCommentAdmin(CommentsAdmin):
+    fieldsets = (
+        (_('Content'),
+           {'fields': ('user', 'mail_me', 'comment')}
+        ),
+        (_('Metadata'), {
+            'classes': ('collapse',),
+            'fields': ('submit_date', 'ip_address', 'is_public', 'is_removed')
+            }
+        ),
+        (_('Other data'), {
+            'classes': ('collapse',),
+            'fields': ('content_type', 'object_pk', 'site')
+            }
+        ),
+     )
+
+    list_display = ('user', 'content_type', 'object_pk', 'ip_address', 'submit_date', 'mail_me', 'is_public', 'is_removed')
+    list_filter = ('submit_date', 'mail_me', 'site', 'is_public', 'is_removed')
+    
+    def get_actions(self, request):
+        actions = super(TerminatorCommentAdmin, self).get_actions(request)
+        actions.pop('flag_comments')
+        actions.pop('approve_comments')
+        actions.pop('remove_comments')
+        return actions
+
+
+admin.site.register(TerminatorComment, TerminatorCommentAdmin)
+
+
+
