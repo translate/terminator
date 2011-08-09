4fe2e319656c488672616b5bcdf9d77a49e421e9
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-08-09 17:26:06 +0200

    Comments: customizing the code for sending emails when saving a comment

--- project/terminator_comments_app/models.py
+++ project/terminator_comments_app/models.py
@@ -10,19 +10,30 @@ class TerminatorComment(Comment):
         return self.content_type.get_object_for_this_type(pk=self.object_pk)
     
     def save(self, *args, **kwargs):
+        # First of all test if object is in database
+        try:
+            object_in_bd = TerminatorComment.objects.get(pk=self.pk)
+        except TerminatorComment.DoesNotExist:
+            changed_or_new = "New"
+        else:
+            changed_or_new = "Changed"
+        # Call the super implementation
         super(TerminatorComment, self).save(*args, **kwargs)
-        # Now send an email to all other users that commented in the current thread
+        # Get the set of emails from all other users that commented in the current thread and want to keep updated
         thread_comments = TerminatorComment.objects.filter(content_type=self.content_type, object_pk=self.object_pk).exclude(user=self.user)
-        
         emails_to_notify_set = set()
         for comment in thread_comments:
             if comment.user.email and comment.mail_me and not comment.user.email in emails_to_notify_set:
                 emails_to_notify_set.add(comment.user.email)
         
         #TODO notificar a todos os outros usuarios subscritos tamén para recibir novas deste glosario
+        # Get the set of emails from users that subscribed to glossary updates
         
+        # Now send an email to all other users that commented in the current thread or have subscribed to the glossary
         if emails_to_notify_set:
-            thread = self.content_type.get_object_for_this_type(pk=self.object_pk)
-            mail_subject = "[Terminator] New message in %s thread for concept #%s" % (thread.language.name, thread.concept.pk)
+            thread = self.comment_thread()
+            mail_subject = "[Terminator] %s message in %s thread for concept #%s" % (changed_or_new, thread.language.name, thread.concept.pk)
             send_mail(mail_subject, self.comment, 'donotreply@donotreply.com', list(emails_to_notify_set), fail_silently=False)
-        
+
+
+
