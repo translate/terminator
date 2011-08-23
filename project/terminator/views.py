f7ca281d1ebf2bd513fd762444ac6ec255b98284
Leandro Regueiro <leandro.regueiro@gmail.com>
2011-08-23 17:52:09 +0200

    Search: merging the advanced_search and search view functions

--- project/terminator/views.py
+++ project/terminator/views.py
@@ -565,61 +565,31 @@ def import_view(request):
 
 def search(request):
     if request.method == 'GET' and 'search_string' in request.GET:
-        search_form = SearchForm(request.GET)
-        if search_form.is_valid():
-            search_results = []
-            try:
-                translation_list = get_list_or_404(Translation, translation_text__iexact=search_form.cleaned_data['search_string'])
-                previous_concept = None
-                for trans in translation_list:
-                    try:
-                        definition = get_object_or_404(Definition, concept=trans.concept, language=trans.language)
-                    except Http404:
-                        definition = None
-                    if not previous_concept is trans.concept.pk:
-                        is_first = True
-                        previous_concept = trans.concept.pk
-                        try:
-                            other_translations = get_list_or_404(Translation.objects.exclude(id=trans.pk), concept=trans.concept)[:7]
-                        except Http404:
-                            other_translations = None
-                    else:
-                        other_translations = None
-                        is_first = False
-                    search_results.append({"translation": trans, "definition": definition, "other_translations": other_translations, "is_first": is_first})
-            except Http404:
-                pass
+        if "advanced" in request.path:
+            search_form = AdvancedSearchForm(request.GET)
         else:
-            search_results = None
-    else:
-        search_form = SearchForm()
-        search_results = None
-    context = {'search_form': search_form, 'search_results': search_results}
-    context['next'] = request.get_full_path()
-    return render_to_response('search.html', context, context_instance=RequestContext(request))
-
-
-
-def advanced_search(request):
-    if request.method == 'GET' and 'search_string' in request.GET:
-        search_form = AdvancedSearchForm(request.GET)
+            search_form = SearchForm(request.GET)
         if search_form.is_valid():
             search_results = []
             try:
-                queryset = Translation.objects.all()
-                if search_form.cleaned_data['filter_by_glossary']:
-                    queryset = queryset.filter(concept__glossary=search_form.cleaned_data['filter_by_glossary'])
-                if search_form.cleaned_data['filter_by_language']:
-                    queryset = queryset.filter(language=search_form.cleaned_data['filter_by_language'])
-                if search_form.cleaned_data['filter_by_part_of_speech']:
-                    queryset = queryset.filter(part_of_speech=search_form.cleaned_data['filter_by_part_of_speech'])
-                if search_form.cleaned_data['filter_by_administrative_status']:
-                    queryset = queryset.filter(administrative_status=search_form.cleaned_data['filter_by_administrative_status'])
-                
-                if search_form.cleaned_data['also_show_partial_matches']:
-                    translation_list = get_list_or_404(queryset, translation_text__icontains=search_form.cleaned_data['search_string'])
+                if "advanced" in request.path:
+                    queryset = Translation.objects.all()
+                    if search_form.cleaned_data['filter_by_glossary']:
+                        queryset = queryset.filter(concept__glossary=search_form.cleaned_data['filter_by_glossary'])
+                    if search_form.cleaned_data['filter_by_language']:
+                        queryset = queryset.filter(language=search_form.cleaned_data['filter_by_language'])
+                    #TODO add filter by process status
+                    if search_form.cleaned_data['filter_by_part_of_speech']:
+                        queryset = queryset.filter(part_of_speech=search_form.cleaned_data['filter_by_part_of_speech'])
+                    if search_form.cleaned_data['filter_by_administrative_status']:
+                        queryset = queryset.filter(administrative_status=search_form.cleaned_data['filter_by_administrative_status'])
+                    
+                    if search_form.cleaned_data['also_show_partial_matches']:
+                        translation_list = get_list_or_404(queryset, translation_text__icontains=search_form.cleaned_data['search_string'])
+                    else:
+                        translation_list = get_list_or_404(queryset, translation_text__iexact=search_form.cleaned_data['search_string'])
                 else:
-                    translation_list = get_list_or_404(queryset, translation_text__iexact=search_form.cleaned_data['search_string'])
+                    translation_list = get_list_or_404(Translation, translation_text__iexact=search_form.cleaned_data['search_string'])
                 
                 previous_concept = None
                 for trans in translation_list:
@@ -643,11 +613,17 @@ def advanced_search(request):
         else:
             search_results = None
     else:
-        search_form = AdvancedSearchForm()
+        if "advanced" in request.path:
+            search_form = AdvancedSearchForm()
+        else:
+            search_form = SearchForm()
         search_results = None
     context = {'search_form': search_form, 'search_results': search_results}
     context['next'] = request.get_full_path()
-    return render_to_response('advanced_search.html', context, context_instance=RequestContext(request))
+    template_name = 'search.html'
+    if "advanced" in request.path:
+        template_name = 'advanced_search.html'
+    return render_to_response(template_name, context, context_instance=RequestContext(request))
 
 
 
