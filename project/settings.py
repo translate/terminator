76ac638874707e6b9b8217316b30e950601a8355
Paul Anderson <anderssakoul@users.noreply.github.com>
2016-07-06 20:34:35 +0100

    Suppress deprecation warning with Django 1.5 - http://django.readthedocs.io/en/latest/releases/1.4.html#request-exceptions-are-now-always-logged

--- project/settings.py
+++ project/settings.py
@@ -174,9 +174,15 @@ SEND_NOTIFICATION_EMAILS = True
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+         'require_debug_false': {
+              '()': 'django.utils.log.RequireDebugFalse'
+         }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
