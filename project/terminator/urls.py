2f7efeed2c0d0b7dc134e295d6e4257861030615
Paul Anderson <anderssakoul@users.noreply.github.com>
2016-07-07 01:27:07 +0100

    django.conf.urls.defaults changed to django.conf.urls to suppress deprecation warning.

--- project/terminator/urls.py
+++ project/terminator/urls.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License along with
 # Terminator. If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.contrib.comments.feeds import LatestCommentFeed
 
 from terminator.feeds import LatestChangesFeed, LatestChangesGenericFeed
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License along with
 # Terminator. If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls.defaults import include, patterns, url
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
 
 
