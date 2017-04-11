2aee42e252b0ac963a907a17a188b1699846b710
Friedel Wolff <friedel@translate.org.za>
2017-04-11 14:44:28 +0200

    Some initial migrations

---
+++ project/terminator_comments_app/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('django_comments', '0003_add_submit_date_index'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='TerminatorComment',
+            fields=[
+                ('comment_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='django_comments.Comment')),
+                ('mail_me', models.BooleanField(default=True)),
+            ],
+            options={
+                'ordering': ('submit_date',),
+                'abstract': False,
+                'verbose_name': 'comment',
+                'verbose_name_plural': 'comments',
+                'permissions': [('can_moderate', 'Can moderate comments')],
+            },
+            bases=('django_comments.comment',),
+        ),
+    ]
