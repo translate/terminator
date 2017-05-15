d0b425a780efcb79ddb56ee51cf2ae9614f0f4e5
Friedel Wolff <friedel@translate.org.za>
2017-05-15 14:52:12 +0200

    admin.autodiscover() is unnecessary since Django 1.7

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import include, patterns, url
 from django.contrib import admin
 
 
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
